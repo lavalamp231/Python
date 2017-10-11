5358d71240549f1b9b934ddd1d503905dd05ddb0
lavalamp231 <andrew.w.simons@gmail.com>
2017-10-11 15:11:29 -0500

    added the rsync_command variable to reduce repetition

--- plex_backup.py
+++ plex_backup.py
@@ -5,20 +5,19 @@ import os
 
 mount_point_path = "/Plex_backup"
 mp = os.path.ismount(mount_point_path)
+rsync_command = ["rsync -av /Storage2/Movies/ /Plex_backup/Movies/", "rsync -av /Storage2/TV-Shows/ /Plex_backup/TV-Shows/"]
 
 def(mount_plexbackup):
 
 	if mp == True:
 		print("No errors. Starting rsync.")
-		os.cmd ("rsync -av /Storage2/TV-Shows/ /Plex_backup/TV-Shows/")
-		os.cmd ("rsync -av /Storage2/Movies/ /Plex_backup/Movies/")
+		os.cmd (rsync_command)
 	else:
 		os.cmd ("mount /Plex_backup")
 		if mp == False:
 			print("NAS was not mounted exiting")
 		else:
 			print("NAS was mounted. Starting rsync now.")
-			os.cmd ("rsync -av /Storage2/TV-Shows/ /Plex_backup/TV-Shows/")
-			os.cmd ("rsync -av /Storage2/Movies/ /Plex_backup/Movies/")			
+			os.cmd (rsync_command)		
 
 		 
