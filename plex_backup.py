a25bae525ae95eacda0976c60413931ee99378c4
lavalamp231 <andrew.w.simons@gmail.com>
2017-10-11 15:16:47 -0500

    fixed a few things

--- plex_backup.py
+++ plex_backup.py
@@ -5,19 +5,20 @@ import os
 
 mount_point_path = "/Plex_backup"
 mp = os.path.ismount(mount_point_path)
+mount_plex = "mount /Plex_backup"
 rsync_command = ["rsync -av /Storage2/Movies/ /Plex_backup/Movies/", "rsync -av /Storage2/TV-Shows/ /Plex_backup/TV-Shows/"]
 
-def(mount_plexbackup):
+def mount_plexbackup():
 
 	if mp == True:
 		print("No errors. Starting rsync.")
-		os.cmd (rsync_command)
+		os.cmd(rsync_command)
 	else:
-		os.cmd ("mount /Plex_backup")
+		os.cmd(mount_plex)
 		if mp == False:
 			print("NAS was not mounted exiting")
 		else:
 			print("NAS was mounted. Starting rsync now.")
-			os.cmd (rsync_command)		
+			os.cmd(rsync_command)		
 
 		 
