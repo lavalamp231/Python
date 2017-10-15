60e422f12530ec35f86b9ce5dde73f8f47068156
bryant <i.am.the.ansible@protonmail.com>
2017-10-15 16:42:22 -0500

    i did another dumb

--- plex_backup.py
+++ plex_backup.py
@@ -12,25 +12,18 @@ def main():
     """ """
 
     mp_is = check_if()
-    if mp_is == False:
-        mount_plexbackup()
+    if mp_is:
         back_up()
-    else:
-        back_up()
-        sys.exit()
 
 def mount_plexbackup():
     """ """
-
     try:
         subprocess.call("mount /Plex_backup", shell=True)
     except Exception as e:
         print("shit wrong: {0}".format(e))
-        sys.exit()
 
 def back_up():
     """ """
-
     subprocess.call("rsync -av /Storage2/Movies/ /Plex_backup/Movies/", shell=True)
     subprocess.call("rsync -av /Storage2/TV-Shows/ /Plex_backup/TV-Shows/", shell=True)
 
@@ -40,6 +33,7 @@ def check_if():
     if mp:
         return True
     else:
+        mount_plexbackup()
         return False
 
 main()
