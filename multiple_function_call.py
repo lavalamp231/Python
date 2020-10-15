47bd1bcaf97afb951e2f3138dd4e705b93811403
lavalamp231 <andrew.w.simons@gmail.com>
2020-10-15 16:08:47 -0500

    cleaning up

--- multiple_function_call.py
+++
@@ -1,8 +0,0 @@
-def describe_pet(animal_type, pet_name):
-	"""Display information about a pet."""
-	print("\nI have a " + animal_type + ".")
-	print("My " + animal_type + "'s name is " + pet_name.title() + ".")
-
-describe_pet('dog', 'chloe')
-describe_pet('dog', 'roxy')
-describe_pet('snake', 'cunt')
