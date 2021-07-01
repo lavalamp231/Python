a76ba3b0b5c8c72b7c0ff95cf8f415138baae597
andrew simons <andrew.w.simons@gmail.com>
2021-06-30 22:16:29 -0500

    update to main

--- nmap_scanner/main.py
+++ nmap_scanner/main.py
@@ -5,13 +5,23 @@ import nmap3
 import pandas as pd
 import socket
 import time
+from datetime import date
 
-# Gathering all the IPs in 192.168.0.* and then appending them into an empty list called ips
+# Get your IP address
+
+local_hostname = socket.gethostname()
+local_ip = socket.gethostbyname(local_hostname)
+
+slocal_ip = local_ip.split(".")
+str_local_subnet = str(slocal_ip[0] + "." + slocal_ip[1] +"."+ slocal_ip[2]+ ".*")
+print(str_local_subnet)
+
+# Gathering all the IPs in local subnet and then appending them into an empty list called ips
 
 ips = []
 nm = nmap3.Nmap()
 nm_scan = nmap3.NmapScanTechniques()
-host_discovery_results = nm_scan.nmap_ping_scan("192.168.0.*")
+host_discovery_results = nm_scan.nmap_ping_scan(str_local_subnet)
 
 for item in host_discovery_results:
     ip_address = item['addresses'][0]['addr']
@@ -62,8 +72,14 @@ for ip in ips:
         state = key['state']
         ports = key['port']
         names = key['service']['name']
-        macad = get_ip_info(ip)['mac_address']
-        vendor_info = get_ip_info(ip)['Vendor']
+        try:
+            macad = get_ip_info(ip)['mac_address']
+        except Exception:
+            macad = "Unknown"
+        try:
+            vendor_info = get_ip_info(ip)['Vendor']
+        except Exception:
+            vendor_info = "Unknown"
         my_dict = {'Hostnames': host_names, 'IPs': ip, 'Protocols': protocols, 'Ports': ports, 'Names': names, 'State': state, 'MACaddress': macad, 'Vendor': vendor_info}
         #print(my_dict)
         df = df.append(my_dict, ignore_index=True)
@@ -71,7 +87,8 @@ for ip in ips:
 
 # Creating a excel sheet from the dataframe. Will create the spreadsheet from current working directory. 
 cwd = os.getcwd()
-excel_location = (cwd +'\\nmap_version_detection.xlsx')
+today = str(date.today())
+excel_location = (cwd +'\\nmap_version_detection_'+ today + '.xlsx')
 
 # putting an exception to deal with if the excel sheet is already openeded.
 try:
