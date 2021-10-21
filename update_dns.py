e4deb2819646227bee62573ab8501072c8aba235
andrew simons <andrew.w.simons@gmail.com>
2021-10-20 20:39:05 -0500

    new update

--- update_dns.py
+++ update_dns.py
@@ -13,36 +13,36 @@ input_hostname = input_hostname + "."
 esxi_com = i + "\\dns_files\\esxi.com.txt"
 
 def check_esxi_com():
-	#d = "/var/named/esxi.com"
-	with open(esxi_com) as x:
-		data = x.read()
-		print(data)		
-		for line in data.split("\n"):
-			if "IN A" in line: 
-			    print(line.split(" "))
-			    list_line = line.split(" ")
-			    if input_hostname in list_line:
-			    	a = input("hostname already exists - would you like to update the hostname in the DNS record? y = yes; n = no")
-			    	if a == int or a != "y" or a != "n":
-			    		a = input("hostname already exists - would you like to update the DNS record? y = yes; n = no")
-			    	elif a == "y":
-			    		line_to_replace = input_hostname + "IN A " + list_line[3]
-			    		return str(line_to_replace)
-			    	elif a == "n":
-			    		exit()
-			    else:
-			    	return False # not replacing; hostname was not found in the file
+    #d = "/var/named/esxi.com"
+    with open(esxi_com) as x:
+        data = x.read()
+        print(data)     
+        for line in data.split("\n"):
+            if "IN A" in line: 
+                print(line.split(" "))
+                list_line = line.split(" ")
+                if input_hostname in list_line:
+                    a = input("hostname already exists - would you like to update the hostname in the DNS record? y = yes; n = no")
+                    if a == int or a != "y" or a != "n":
+                        a = input("hostname already exists - would you like to update the DNS record? y = yes; n = no")
+                    elif a == "y":
+                        line_to_replace = input_hostname + "IN A " + list_line[3]
+                        return str(line_to_replace)
+                    elif a == "n":
+                        exit()
+                else:
+                    return False # not replacing; hostname was not found in the file
 
 result = check_esxi_com()
 
 def update_esxi_com():
-	with open(esxi_com, "a+") as x:
-		if result == False: # adding entry
-			ip = input("What is the ip you are adding for " + input_hostname)
-			x.write("\n" + input_hostname + " IN A" + ip)
-		else: # replacing the line
-		    ip = input("Replacing " + input_hostname + "ip. What is the new ip:")
-			print("replacing " + input_hostname + "'s IP in esxi.com")
-			x.replace(result, input_hostname + " IN A " + ip)
+    with open(esxi_com, "a+") as x:
+        if result == False: # adding entry
+            ip = input("What is the ip you are adding for " + input_hostname)
+            x.write("\n" + input_hostname + " IN A" + ip)
+        else: # replacing the line
+            ip = input("Replacing " + input_hostname + "ip. What is the new ip:")
+            print("replacing " + input_hostname + "'s IP in esxi.com")
+            x.replace(result, input_hostname + " IN A " + ip)
 
 
