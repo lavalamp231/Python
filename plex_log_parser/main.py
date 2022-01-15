6ebb50009c170d5144b12a5a016939cfcc47703a
lavalamp231 <root@ansible.esxi.com>
2022-01-14 22:34:05 -0600

    cleaning up

--- plex_log_parser/main.py
+++ plex_log_parser/main.py
@@ -2,13 +2,6 @@ import os
 import sys
 
 dir_path = r"/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Logs/"
-file_path = r"/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Logs/Plex Media Server.log"
-#log_dir = '/var/lib/plexmediaserver/Library/Application\ Support/Plex\ Media\ Server/Logs/Plex\ Media\ Server.1.log'
-# with open(file_path, "r") as r:
-#     for line in r:
-#         if "gmail" in line:
-#             print(line)
-
 
 os.chdir(dir_path)
 
@@ -23,7 +16,7 @@ def read_text_file(file_path):
 for file in os.listdir():
     # Check whether file is in text format or not
     if "Plex Media Server" in file:
-        print(file)
+        #print(file)
         file_path = f"{dir_path}{file}"
   
         # call read text file function
