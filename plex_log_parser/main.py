6e30bf8ca91be1556f4e3594c789aed017cef447
root <root@plex.esxi.com>
2022-01-15 11:04:50 -0600

    found correct thing to search for. Need to do regex magic to extract correct strings

--- plex_log_parser/main.py
+++ plex_log_parser/main.py
@@ -5,10 +5,13 @@ dir_path = r"/var/lib/plexmediaserver/Library/Application Support/Plex Media Ser
 
 os.chdir(dir_path)
 
+list_of_movies_watched = []
+user_dict = dict()
+
 def read_text_file(file_path):
     with open(file_path, 'r') as f:
         for line in f:
-            listofwords = ["gmail", "itary"]
+            listofwords = ["WAN", "title="]
             if all(x in line for x in listofwords):
                 print(line)
 
@@ -16,8 +19,10 @@ def read_text_file(file_path):
 for file in os.listdir():
     # Check whether file is in text format or not
     if "Plex Media Server" in file:
-        #print(file)
         file_path = f"{dir_path}{file}"
-  
         # call read text file function
         read_text_file(file_path)
+
+# create a dict with user. Need item played, IP, user (key), date, x-plex-device-name
+
+
