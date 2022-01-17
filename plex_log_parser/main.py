ed5799634c1a938d2e693ab81d73616be4894378
lavalamp231 <root@ansible.esxi.com>
2022-01-17 13:43:33 -0600

    updated to add onto results.json instead of printing output and redirecting

--- plex_log_parser/main.py
+++ plex_log_parser/main.py
@@ -1,13 +1,15 @@
 import os
-import sys
 import re
+import json
 
 dir_path = r"/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Logs/"
+json_file = r"/Misc/Python_git/plex_log_parser/results.json"
 
 os.chdir(dir_path)
 
 # create a dict with user. Need item played, IP, user (key), date, x-plex-device-name
 user_dict = dict()
+
 
 def read_text_file(file_path):
     with open(file_path, 'r') as f:
@@ -26,9 +28,13 @@ for file in os.listdir():
     # Check whether file is in text format or not
     if "Plex Media Server" in file:
         file_path = f"{dir_path}{file}"
-        # call read text file function
         read_text_file(file_path)
 
-# create a dict with user. Need item played, IP, user (key), date, x-plex-device-name
 
-print(user_dict)
+#need to read results.json file to work off of then add new entries. 
+
+with open(json_file, 'r+') as f:
+    d = json.load(f)
+    d.update(user_dict)
+    f.seek(0)
+    json.dump(d, f)
